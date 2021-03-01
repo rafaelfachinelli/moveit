c98fc9e663b0cacfbbad7ac4cfc7eb0db9b056e6
rafaelfachinelli <rafael.fachinelli@fatec.sp.gov.br>
2021-02-28 23:59:46 -0300

    feat(index): index page finished

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,61 @@
-import { ExperienceBar } from "../components/ExperienceBar";
+import Head from "next/head";
+import { GetServerSideProps } from "next";
 
-export default function Home() {
+import { CompletedChallenges } from "../components/CompletedChallenges";
+import { Countdown } from "../components/Countdown";
+import { ExperienceBar } from "../components/ExperienceBar";
+import { Profile } from "../components/Profile";
+import { ChallengeBox } from "../components/ChallengeBox";
+import { ChallengesProvider } from "../contexts/ChallengeContext";
+import { CountdownProvider } from "../contexts/CountdownContext";
+
+import styles from "../styles/pages/Home.module.css";
+
+interface HomeProps {
+  level: number;
+  currentExperience: number;
+  challengesCompleted: number;
+}
+
+export default function Home(props: HomeProps) {
   return (
-    <div className="container">
-      <ExperienceBar />
-    </div>
+    <ChallengesProvider
+      level={props.level}
+      currentExperience={props.currentExperience}
+      challengesCompleted={props.challengesCompleted}
+    >
+      <div className={styles.container}>
+        <Head>
+          <title>Início | move.it</title>
+        </Head>
+
+        <ExperienceBar />
+
+        <CountdownProvider>
+          <section>
+            <div>
+              <Profile />
+              <CompletedChallenges />
+              <Countdown />
+            </div>
+            <div>
+              <ChallengeBox />
+            </div>
+          </section>
+        </CountdownProvider>
+      </div>
+    </ChallengesProvider>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async (context) => {
+  const { level, currentExperience, challengesCompleted } = context.req.cookies;
+
+  return {
+    props: {
+      level: Number(level),
+      currentExperience: Number(currentExperience),
+      challengesCompleted: Number(challengesCompleted)
+    }
+  };
+};
